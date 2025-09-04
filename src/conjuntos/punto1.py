e795bc4b1a17523ad6cfbe686f69be9148b15829
av164702gmail.com <av164702gmail.com>
2025-09-04 14:20:09 -0500

    Inicio del trabajo

---
+++ src/conjuntos/punto1.py
@@ -0,0 +1,6 @@
+# Aquí creamos un conjunto.
+# Los conjuntos guardan elementos únicos. Por eso, el segundo "rojo" se ignora.
+colores = {"rojo", "verde", "azul", "rojo"}
+
+# La función 'print' muestra el conjunto sin los elementos repetidos.
+print(colores)  # Imprime: {'verde', 'azul', 'rojo'}
